a8f38e38459c63de12ba4d6bff06d19cd717ff47
--replace_all <shabbirsaifee91@gmail.com>
2019-04-02 22:41:59 -0400

    equality matcher: equal method

--- spec/equality_matchers_spec.rb
+++ spec/equality_matchers_spec.rb
@@ -17,4 +17,18 @@ RSpec.describe 'equality matchers' do
       expect(a).not_to eql(b)
     end
   end
+
+  describe 'equal and be matcher' do
+    let(:c) { [1, 2, 3]}
+    let(:d) { [1, 2, 3] }
+    let(:e) { c }
+
+    it 'cares about object identity' do
+      expect(c).to eq(d)
+      expect(c).to eql(d)
+
+      expect(c).to equal(e)
+      expect(c).not_to equal(d)
+    end
+  end
 end
