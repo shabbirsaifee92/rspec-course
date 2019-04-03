01ffe21e5d32ac36a4a9471fe8d932a89349aa28
= <=>
2019-04-02 20:43:09 -0400

    Rspec practice

---
+++ spec/shared_examples_spec.rb
@@ -0,0 +1,31 @@
+class SausageLink 
+  def length
+    3
+  end
+end
+
+shared_examples 'a Ruby object with 3 elements' do
+  it 'should have length 3' do
+    expect(subject.length).to eq 3
+  end
+end
+
+RSpec.describe Array do
+  subject { [1,2,3] }
+  include_examples 'a Ruby object with 3 elements'
+end
+
+RSpec.describe String do
+  subject { 'abc' }
+  include_examples 'a Ruby object with 3 elements'
+end
+
+RSpec.describe Hash do
+  subject { { a: 1, b: 2, c: 3} }
+  include_examples 'a Ruby object with 3 elements'
+end
+
+RSpec.describe SausageLink do
+  subject { described_class.new }
+  include_examples 'a Ruby object with 3 elements'
+end
