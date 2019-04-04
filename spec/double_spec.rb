e9e36e977e3f05510c6b6ba69023931a1539be1d
--replace_all <shabbirsaifee91@gmail.com>
2019-04-04 09:26:51 -0400

    double method

---
+++ spec/double_spec.rb
@@ -0,0 +1,22 @@
+RSpec.describe 'a random double' do
+  it 'only allows defined method to be invoked' do
+    stuntman = double('Mr. Danger', fall_off_ladder: 'Ouch', light_on_fire: true)
+    
+    expect(stuntman.fall_off_ladder).to eq('Ouch')
+    expect(stuntman.light_on_fire).to be true
+  end
+
+  it '' do
+    stuntman = double('Mr Danger')
+    allow(stuntman).to receive(:fall_off_ladder).and_return('Ouch')
+
+    expect(stuntman.fall_off_ladder).to eq('Ouch')
+  end
+
+  it '' do
+    stuntman = double('Mr. Danger')
+    allow(stuntman).to receive_messages(fall_off_ladder: 'Ouch', light_on_fire: true)
+    expect(stuntman.fall_off_ladder).to eq('Ouch')
+    expect(stuntman.light_on_fire).to be true
+  end
+end
